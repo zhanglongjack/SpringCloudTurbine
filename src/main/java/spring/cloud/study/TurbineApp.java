52ac2124c08bcdab0855b5e5743bb8cebb1c8240
zhanglongjack <364666387@qq.com>
2018-05-20 19:30:46 +0800

    test

--- src/main/java/spring/cloud/study/TurbineApp.java
+++ src/main/java/spring/cloud/study/TurbineApp.java
@@ -1,7 +1,6 @@
 package spring.cloud.study;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
@@ -13,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
  *
  */
 @Configuration
-@EnableAutoConfiguration
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableTurbine
